a0869fc5d0fcc3ad92b50e4dac723976e53370bb
Shannon <shaw0106@algonquinlive.com>
2016-03-11 16:49:58 -0500

    started

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var $header = $('header');
+var $h1 = $('h1');
+var $main = $('main');
+var $p = $('p:first-child');
+var $h2 = $('h2');
+var $ul = $('ul');
+var $img = $('img');
+var $firstItem = $('ul:first-child');
+var $lastItem = $('ul:last-child');
+var $dl = $('dl');
+var $dt = $('dt');
+
+$header.addClass('.masthead');
